66d9a0fce53c6c4fecbac78eb9eaa3c3ade80e5b
ThibautRousselet <tibo25300@gmail.com>
2020-04-15 19:46:06 +0200

    Initial Commit

---
+++ BatterieLampe.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+//Cette classe gère la batterie de la lampe torche du joueur
+//La lampe peut etre rechargee en secouant le controleur et a 3 etats possibles :
+// - Recharge : le joueur secoue la lampe et la batterie augmente, la lumiere devient bleue pendant 2 secondes quand la batterie est pleine
+// - Surcharge : Le joueur appuie sur un bouton pour activer ce mode qui rend la lumiere rouge et mortelle pour les ennemis mais consomme plus de batterie
+// - Decharge : La batterie est vide donc la lumiere est eteinte
+// - Normal : Eclaire avec une lumiere blanche dans les autres cas
+
+public class BatterieLampe : MonoBehaviour
+{
+    public float Batterie;
+    public float MaxBatterie;
+
+    public Light lumiere;
+    public SteamVR_Action_Boolean IsSurcharge = null;
+
+    private float timerBatteriePleine;
+    private Vector3 posActuel;
+    private Vector3 posPrecedente;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lumiere = GetComponentInChildren<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        posActuel = this.transform.position;
+
+        //Si on appuie sur le bouton de surcharge, on vide la batterie plus vite et la lumiere devient rouge
+        if (IsSurcharge.state && Batterie>0)
+        {
+            lumiere.color = Color.red;
+            lumiere.intensity = 3;
+            Batterie-=0.5f;
+        }
+        //On teste si la lampe est secouée
+        else if (Vector3.Distance(posActuel, posPrecedente)>0.2)
+        {
+            //on recharge la lampe si c est le cas
+            if (Batterie < MaxBatterie)
+            {
+                Batterie+=10;
+            } 
+            //Si la batterie est pleine, la lumiere devient bleue
+            else if (timerBatteriePleine <= 0)
+            {
+                timerBatteriePleine = 2;
+                lumiere.color = Color.cyan;
+                lumiere.intensity = 1;
+            }
+        } 
+        else
+        //Sinon la lumiere est blanche
+        {
+            lumiere.color = Color.white;
+            lumiere.intensity = 1;
+        }
+
+        //On vide la batterie a chaque update
+        Batterie -= Time.deltaTime;
+        //On vide aleatoirement pour faire varier la duree de vie de la batterie
+        if (Random.value > 0.995)
+        {
+            Batterie--;
+        }
+
+        //Plus de lumiere si la batterie est vide
+        if (Batterie < 0)
+        {
+            Batterie = 0;
+            lumiere.enabled = false;
+            lumiere.intensity = 1;
+
+        } else
+        {
+            lumiere.enabled = true;
+        }
+
+        //On met a jour les valeurs pour le prochain appel
+        posPrecedente = posActuel;
+        timerBatteriePleine -= Time.deltaTime;
+    }
+}
---
+++ DetectionFiole.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static AttributsFiole;
+
+//Le but du jeu et de ramener des fioles de la couleur demandee
+//Cette classe permet de detecter le placement d une dans le recipient dediee 
+// A chaque fois que le joueur amene la bonne fiole, le score augmente de 1, la carte s' assombrit et des zombies apparaissent pour compliquer le jeu
+public class DetectionFiole : MonoBehaviour
+{
+    private Couleur couleurCible;
+    private int score = 0; //nb de fioles ramenées
+
+    public ZombieSpawn ZombieSpawn;
+
+    private Collider autreObjet;//Collider de l objet place dans le recipient
+
+    public GameObject HUD = null;
+    private UnityEngine.UI.Text hudTexte = null;
+
+    public GameObject lumiereCouloirs; //Collection de GroupeLampes de chaque couloir
+    private List<GroupeLampe> groupeLampeCouloirs = new List<GroupeLampe>();
+    public GroupeLampe lobby;
+    public GroupeLampe salle1;
+    public GroupeLampeSecours secoursLobby;
+    public GroupeLampeSecours secoursSalle1;
+
+    void Start()
+    {
+        hudTexte = HUD.GetComponentInChildren<Text>();
+        SetCibleAleatoire();
+        ZombieSpawn = this.GetComponent<ZombieSpawn>();
+
+        //Place les groupes de lumieres dans une liste
+        foreach (GroupeLampe groupeL in lumiereCouloirs.GetComponentsInChildren<GroupeLampe>())
+        {
+            groupeLampeCouloirs.Add(groupeL);
+        }
+    }
+
+    //Appelee quand un objet est placee dans le recipient
+    private void OnTriggerEnter(Collider collider)
+    {
+        autreObjet = collider;
+        Couleur CouleurAutre = autreObjet.gameObject.GetComponent<AttributsFiole>().CouleurFiole;
+
+        //Compare la couleur cible a celle de l'objet place
+        if (couleurCible == CouleurAutre)
+        {
+            OnDetection();
+        }
+
+        //On detruit l'objet dans tous les cas
+        Destroy(autreObjet.gameObject);
+    }
+
+    void Update()
+    {
+        //On peut passer a la phase suivante en appuyant sur p pour tester
+        if (Input.GetKeyDown("p"))
+        {
+            OnDetection();
+        }
+    }
+
+    //On passe a la phase suivante quand la bonne fiole est ammenee
+    private void OnDetection()
+    {
+        score++;
+        switch (score)
+        {
+            //Chaque phase applique des modifications sur la map
+            case 1:
+                Phase1();
+                break;
+            case 2:
+                Phase2();
+                break;
+            case 3:
+                Phase3();
+                break;
+            case 4:
+                Phase4();
+                break;
+
+        }
+    }
+
+    //A chaque phase, on eteind plus de lumieres et on fait apparaitre des zombies
+    private void Phase1()
+    {
+        //Fait clignoter 3 couloirs
+        groupeLampeCouloirs[2].setClignotement(true);
+        groupeLampeCouloirs[3].setClignotement(true);
+        groupeLampeCouloirs[4].setClignotement(true);
+
+        //spawn 10 zombies
+        ZombieSpawn.zombieSpawn(10);
+    }
+
+    private void Phase2()
+    {
+        //On eteint la salle 1
+        secoursSalle1.SetIntensite(1);
+        salle1.SetIntensite(0);
+
+        ZombieSpawn.zombieSpawn(15);
+
+        groupeLampeCouloirs[0].SetIntensite(0);
+        groupeLampeCouloirs[3].SetIntensite(0);
+
+        groupeLampeCouloirs[5].setClignotement(true);
+        groupeLampeCouloirs[6].setClignotement(true);
+    }
+
+    private void Phase3()
+    {
+        groupeLampeCouloirs[2].SetIntensite(0);
+        groupeLampeCouloirs[3].SetIntensite(0);
+        groupeLampeCouloirs[4].SetIntensite(0);
+        groupeLampeCouloirs[5].SetIntensite(0);
+        groupeLampeCouloirs[6].SetIntensite(0);
+        groupeLampeCouloirs[7].SetIntensite(0);
+
+        lobby.SetIntensite(0);
+        secoursLobby.SetIntensite(1);
+
+        ZombieSpawn.zombieSpawn(20);
+
+    }
+
+    //Apres avoir ramene 3 fioles le joueur gagne
+    private void Phase4()
+    {
+        hudTexte.text = "VOUS AVEZ GAGNE";
+    }
+
+    private void SetCible(Couleur couleur)
+    {
+        couleurCible = couleur;
+        hudTexte.text = "Objectif : \n Ramener une fiole " + couleurCible.ToString();
+    }
+
+    //Choisi aleatoirement une couleur cible et l affiche
+    private void SetCibleAleatoire()
+    {
+        couleurCible = GenereCouleurAlea();
+        hudTexte.text = "Objectif : \n Ramener une fiole " + couleurCible.ToString();
+    }
+
+    //Permet de generer une couleur aléatoire
+    private Couleur GenereCouleurAlea()
+    {
+        Couleur coul;
+        float rand = Random.Range(0.0f, 5.0f);
+        if (rand < 1.0f)
+            coul = Couleur.Blanc;
+        else if (rand < 2.0f)
+            coul = Couleur.Bleu;
+        else if (rand < 3.0f)
+            coul = Couleur.Jaune;
+        else if (rand < 4.0f)
+            coul = Couleur.Rouge;
+        else
+            coul = Couleur.Vert;
+        return coul;
+    }
+}
